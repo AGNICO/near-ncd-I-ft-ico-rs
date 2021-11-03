fab668f0581a44e19d2ac4231a2bdc11967c17ec
Jan Palička <info@agnico.net>
2021-11-03 17:28:36 +0100

    Create, remove, view ICOs

--- ft/src/lib.rs
+++ ft/src/lib.rs
@@ -1,8 +1,8 @@
 /*!
 Fungible Token implementation with JSON serialization.
 NOTES:
-  - The maximum balance value is limited by U128 (2**128 - 1).
-  - JSON calls should pass U128 as a base-10 string. E.g. "100".
+  - The maximum balance value is limited by u128 (2**128 - 1).
+  - JSON calls should pass u128 as a base-10 string. E.g. "100".
   - The contract optimizes the inner trie structure by hashing account IDs. It will prevent some
     abuse of deep tries. Shouldn't be an issue, once NEAR clients implement full hashing of keys.
   - The contract tracks the change in storage before and after the call. If the storage increases,
@@ -20,7 +20,7 @@ use near_contract_standards::fungible_token::metadata::{
 };
 use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LazyOption;
+use near_sdk::collections::{UnorderedMap, LazyOption};
 use near_sdk::json_types::{ValidAccountId, U128};
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
 
@@ -31,16 +31,26 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     token: FungibleToken,
     metadata: LazyOption<FungibleTokenMetadata>,
+    icos: UnorderedMap<u128, u128> // new
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
+// Restrict some functions only to owner - new
+fn assert_self() {
+    assert_eq!(
+        env::current_account_id(),
+        env::predecessor_account_id(),
+        "Can only be called by owner"
+    );
+}
 
 #[near_bindgen]
 impl Contract {
     /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
-    pub fn new_default_meta(owner_id: ValidAccountId, total_supply: U128) -> Self {
+    pub fn new_default_meta(owner_id: ValidAccountId, total_supply: u128) -> Self {
         Self::new(
             owner_id,
             total_supply,
@@ -61,18 +71,49 @@ impl Contract {
     #[init]
     pub fn new(
         owner_id: ValidAccountId,
-        total_supply: U128,
-        metadata: FungibleTokenMetadata,
+        total_supply: u128,
+        metadata: FungibleTokenMetadata
     ) -> Self {
         assert!(!env::state_exists(), "Already initialized");
         metadata.assert_valid();
         let mut this = Self {
             token: FungibleToken::new(b"a".to_vec()),
             metadata: LazyOption::new(b"m".to_vec(), Some(&metadata)),
+            icos: UnorderedMap::new(b"i".to_vec())
         };
         this.token.internal_register_account(owner_id.as_ref());
         this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
         this
+    }
+
+    /// Create new ICO - new
+    pub fn new_ico(&mut self, near_price: u128, supply: u128) {
+        assert!(env::state_exists(), "Mint initial token supply");
+        assert_self();
+        let total_ico_supply: u128 = self.icos.values().sum();
+        assert!(supply <= self.token.total_supply - total_ico_supply, "Not enough tokens for ICO");
+        self.icos.insert(&near_price, &supply);
+    }
+
+    /// Remove ICO - new
+    pub fn remove_ico(&mut self, near_price: u128) {
+        assert!(env::state_exists(), "Mint initial token supply");
+        assert_self();
+        self.icos.remove(&near_price);
+    }
+
+    /// View ICO - new
+    pub fn get_ico(&self, near_price: u128) -> Option<u128> {
+        return self.icos.get(&near_price);
+    }
+
+    /// Get paginated ICOs - new
+    pub fn get_icos(&self, from_index: u64, limit: u64) -> Vec<(u128, u128)> {
+        let keys = self.icos.keys_as_vector();
+        let values = self.icos.values_as_vector();
+        (from_index..std::cmp::min(from_index + limit, self.icos.len()))
+            .map(|index| (keys.get(index).unwrap(), values.get(index).unwrap()))
+            .collect()
     }
 
     fn on_account_closed(&mut self, account_id: AccountId, balance: Balance) {
